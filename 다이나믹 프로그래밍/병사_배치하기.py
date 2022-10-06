25c3ddb47b54f6e3e23181e0c7142174ba5b5011
panorama713 <yaiba713@gmail.com>
2022-10-06 23:50:42 +0900

    10/06

---
+++ 다이나믹 프로그래밍/병사_배치하기.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+arr.reverse()
+
+dp = [1] * n
+
+# 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
+for i in range(1, n):
+    for j in range(0, i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
